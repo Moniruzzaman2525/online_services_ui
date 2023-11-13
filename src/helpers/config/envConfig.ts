71bead7f65053d79abd6eaf7172b65a2fe0313c3
MD MONIRUZZAMAN <96796294+Moniruzzaman2525@users.noreply.github.com>
2023-11-13 10:02:59 +0600

    change url

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,9 +1,9 @@
 export const getBaseUrl = (): string => {
   // return process.env.BACKEND_URL || "http://localhost:5000/api/v1";
-  return "https://online-services-gamma.vercel.app/api/v1";
+  return "https://online-work-platform-backend.vercel.app/api/v1";
   // return "http://localhost:5000/api/v1";
 };
 
 export const envConfig = {
-  siteUrl: process.env.NEXTAUTH_URL || "https://online-services-ui.vercel.app/",
+  siteUrl: process.env.NEXTAUTH_URL || "http://localhost:3000",
 };
